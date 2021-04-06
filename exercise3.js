419564d14786f4e0e550659d45b5c73f60429d70
Malcolm Curtis <43641457+Malcmalc2@users.noreply.github.com>
2021-04-06 19:04:49 -0400

    Add files via upload

---
+++ exercise3.js
@@ -0,0 +1,35 @@
+
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.  
+
+
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+// Addition
+var firstNumber = prompt("What is your First Number?");
+var secondNumber = prompt("What is your Second Number?");
+var sum = Number(firstNumber) + Number(secondNumber);
+alert(firstNumber + "plus" + secondNumber + "=" + sum);
+
+// Substraction
+var firstNumber = prompt("What is your First Number?");
+var secondNumber = prompt("What is your Second Number?");
+var sum = Number(firstNumber) - Number(secondNumber);
+alert(firstNumber + "minus" + secondNumber + "=" + sum);
+
+// Multiply
+var firstNumber = prompt("What is your First Number?");
+var secondNumber = prompt("What is your Second Number?");
+var sum = Number(firstNumber) * Number(secondNumber);
+alert(firstNumber + "times" + secondNumber + "=" + sum);
+
+// Divide
+var firstNumber = prompt("What is your First Number?");
+var secondNumber = prompt("What is your Second Number?");
+var sum = Number(firstNumber) / Number(secondNumber);
+alert(firstNumber + "divided by" + secondNumber + "=" + sum);
+
+
